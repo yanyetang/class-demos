c4da597ec94eca3db6c1f6c25be0fb964d040b23
Yanye Tang <tangyanye@gmail.com>
2025-11-27 13:21:50 +0000

    end of first class Nov27

---
+++ JQUERY/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("button.continue").html("Next Step...");
+});
+
+$("p").css("background-color", "lightBlue");
+$("p#para2").css("font-weight", "bold");
+
+console.log($("p#para2").css("font-size"));
---
+++ Typescript/src/greeter.ts
@@ -0,0 +1,7 @@
+function greet(person: string): string {
+  return "Hello" + person;
+}
+
+let person = "Jane";
+
+console.log(greet(person));
